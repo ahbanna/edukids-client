920e8d9a175adc70eddf16e393f2c9d5507f68c1
Al Hasanul Banna <tutul5800@gmail.com>
2023-05-19 01:52:00 +0600

    404 page design

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./PageNotFound.css";
+import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return (
-    <div>
+    <div className="error-area">
       <div className="container">
         <div className="row">
-          <div className="col-lg-6"></div>
-          <div className="col-lg-6"></div>
+          <div className="col-lg-6">
+            <img src="https://studio.uxpincdn.com/studio/wp-content/uploads/2021/06/10-error-404-page-examples-for-UX-design.png.webp" />
+          </div>
+          <div class="col-lg-6 error-text">
+            <h3>Page Not Found</h3>
+            <div class="back-to-home-btn">
+              <button>
+                <Link to="/">Go To Home Page</Link>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
