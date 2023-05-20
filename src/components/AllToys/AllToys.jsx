a9f9802cc300166cc83c7eaed348c5d019697020
Al Hasanul Banna <tutul5800@gmail.com>
2023-05-20 16:59:39 +0600

    search filed implemented on all toys page

--- src/components/AllToys/AllToys.jsx
+++ src/components/AllToys/AllToys.jsx
@@ -1,4 +1,46 @@
-import React from "react";
+// import React from "react";
+// import useTitle from "../../hooks/useTitle";
+// import { useLoaderData } from "react-router-dom";
+// import AllToysTable from "../AllToysTable/AllToysTable";
+// import { Table } from "react-bootstrap";
+// import "./AllToys.css";
+
+// const AllToys = () => {
+//   const toys = useLoaderData();
+//   useTitle("All Toys");
+//   return (
+//     <div className="container">
+//       <div className="section-title">
+//         <p>At A Glance</p>
+//         <h3>All Toys</h3>
+//       </div>
+//       <div className="all-toys-table">
+//         <Table striped bordered hover>
+//           <thead>
+//             <tr>
+//               <th>Seller Name</th>
+//               <th>Toy Name</th>
+//               <th>Sub Category</th>
+//               <th>Price</th>
+//               <th>Available Quantity</th>
+//               <th>Details</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {toys.map((toy) => (
+//               <AllToysTable key={toy._id} toy={toy}></AllToysTable>
+//             ))}
+//           </tbody>
+//         </Table>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AllToys;
+// THE ABOVE CODE IS WITHOUT SEARCH FIELD
+
+import React, { useState } from "react";
 import useTitle from "../../hooks/useTitle";
 import { useLoaderData } from "react-router-dom";
 import AllToysTable from "../AllToysTable/AllToysTable";
@@ -8,11 +50,26 @@ import "./AllToys.css";
 const AllToys = () => {
   const toys = useLoaderData();
   useTitle("All Toys");
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className="container">
       <div className="section-title">
         <p>At A Glance</p>
         <h3>All Toys</h3>
+      </div>
+      <div className="search-area">
+        <input
+          type="text"
+          placeholder="Search Toys By Name"
+          value={searchTerm}
+          onChange={handleChange}
+        />
       </div>
       <div className="all-toys-table">
         <Table striped bordered hover>
@@ -27,9 +84,13 @@ const AllToys = () => {
             </tr>
           </thead>
           <tbody>
-            {toys.map((toy) => (
-              <AllToysTable key={toy._id} toy={toy}></AllToysTable>
-            ))}
+            {toys
+              .filter((toy) =>
+                toy.toyName.toLowerCase().includes(searchTerm.toLowerCase())
+              )
+              .map((toy) => (
+                <AllToysTable key={toy._id} toy={toy}></AllToysTable>
+              ))}
           </tbody>
         </Table>
       </div>
